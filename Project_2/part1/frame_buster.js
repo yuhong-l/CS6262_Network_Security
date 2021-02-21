08ea46592ef905e0f1707ee4055c06335d1ac5d7
Irfan Malik <imalik30@gatech.edu>
2021-02-21 15:45:33 -0500

    Testing out part 1.2

--- Project_2/part1/frame_buster.js
+++ Project_2/part1/frame_buster.js
@@ -1,1 +1,21 @@
 // Write your code below
+
+console.log("my extension running : IM ");
+
+
+
+
+var kill_bust = 0
+
+window.onbeforeunload =  function() { kill_bust++ }
+
+setInterval(  function() {
+
+	if ( kill_bust  >  0  ) {
+		kill_bust = kill_bust -2;
+		window.top.location =  'http://clients1.google.com/generate_204'
+	}	
+
+} , 1  ); 
+
+
